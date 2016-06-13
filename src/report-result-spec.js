3216a709e25a46d2de5a4c50433100b4b6dff188
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-06-13 08:58:13 -0400

    feat(ci): semantic release

--- src/report-result-spec.js
+++ src/report-result-spec.js
@@ -1,7 +1,9 @@
 'use strict'
 
 /* global describe, it */
-describe('', () => {
-  it('', () => {
+describe('report result', () => {
+  const report = require('.')
+  it('has success', () => {
+    report.success('info')('done')
   })
 })
